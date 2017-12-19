4136870944b93f7ce1d14f351c8def8432956497
FastStim <student2063@yandex.ru>
2017-12-20 01:56:43 +0300

    add Egor task

--- mgk/src/main.cpp
+++ mgk/src/main.cpp
@@ -1,52 +1,73 @@
 #include "main.h"
 
-/*
-Матрица
-
-0.008 0.100 0.500 44000 500 2800000 0.3 30
-0.010, 0.0625, 0.125, 70000, 700, 3000000, 0.8, 45
-0.010, 0.1111, 0.200, 47000, 500, 2550000, 0.2, 19
-0.010, 0.1250, 0.050, 49000, 600, 2600000, 0.6, 20
-0.008, 0.2000, 0.143, 45000, 400, 2500000, 0.3, 13
-0.003, 0.2500, 0.167, 80000, 600, 3300000, 0.1, 92
-0.001, 0.7500, 0.038, 85000, 600, 2500000, 0.9, 11
-
-*/
-
 int main()
 {
-	float* krit = new float[8];
-	float** mat = new float*[8];
-	float** step = new float*[8];
-	float** step2 = new float*[8];
+	int size = 5;
+
+	string* name = new string[size];
+	float* krit = new float[size];
+	float** mat = new float*[size];
+	float** step = new float*[size];
+	float** step2 = new float*[size];
+	string curName = "";
 	
-	mat[0] = new float[8] {0.008, 0.100,  0.500, 44000, 500, 2800000, 0.3, 30};
-	mat[1] = new float[8] {0.010, 0.0625, 0.125, 70000, 700, 3000000, 0.8, 45};
-	mat[2] = new float[8] {0.010, 0.1111, 0.200, 47000, 500, 2550000, 0.2, 19};
-	mat[3] = new float[8] {0.010, 0.1250, 0.050, 49000, 600, 2600000, 0.6, 20};
-	mat[4] = new float[8] {0.008, 0.2000, 0.143, 45000, 400, 2500000, 0.3, 13};
-	mat[5] = new float[8] {0.003, 0.2500, 0.167, 80000, 600, 3300000, 0.1, 92};
-	mat[6] = new float[8] {0.001, 0.7500, 0.038, 85000, 600, 2500000, 0.9, 11};
+	//Default task
 
-	krit[0] = 0.01;
-	krit[1] = 0.1;
-	krit[2] = 0.038;
-	krit[3] = 44000;
-	krit[4] = 400;
-	krit[5] = 2500000;
-	krit[6] = 0.3;
-	krit[7] = 10;
+	// mat[0] = new float[size] {0.008, 0.100,  0.500, 44000, 500, 2800000, 0.3, 30};
+	// mat[1] = new float[size] {0.010, 0.0625, 0.125, 70000, 700, 3000000, 0.8, 45};
+	// mat[2] = new float[size] {0.010, 0.1111, 0.200, 47000, 500, 2550000, 0.2, 19};
+	// mat[3] = new float[size] {0.010, 0.1250, 0.050, 49000, 600, 2600000, 0.6, 20};
+	// mat[4] = new float[size] {0.008, 0.2000, 0.143, 45000, 400, 2500000, 0.3, 13};
+	// mat[5] = new float[size] {0.003, 0.2500, 0.167, 80000, 600, 3300000, 0.1, 92};
+	// mat[6] = new float[size] {0.001, 0.7500, 0.038, 85000, 600, 2500000, 0.9, 11};
 
-	// 
+	// krit[0] = 0.01;
+	// krit[1] = 0.1;
+	// krit[2] = 0.038;
+	// krit[3] = 44000;
+	// krit[4] = 400;
+	// krit[5] = 2500000;
+	// krit[6] = 0.3;
+	// krit[7] = 10;
+
+	// name[0] = "Смена";
+	// name[1] = "Час Пик";
+	// name[2] = "Невское время";
+	// name[3] = "Вечерний ПБ";
+	// name[4] = "СПБ ведомости";
+	// name[5] = "Деловой ПБ";
+	// name[6] = "Реклама - Шанс";
+
+	// Egor task
+
+	mat[0] = new float[size] {3.1, 128, 200, 150, -4690};
+	mat[1] = new float[size] {2.0, 32,  15,  5,   -750 };
+	mat[2] = new float[size] {3.0, 32,  90,  20,  -1070};
+	mat[3] = new float[size] {2.0, 8,   27,  12,  -570 };
+	mat[4] = new float[size] {3.0, 128, 150, 150, -3220};
+
+	krit[0] = 3.0;
+	krit[1] = 32;
+	krit[2] = 90;
+	krit[3] = 20;
+	krit[4] = -4690;
+
+	name[0] = "SanDisk Extreme Go";
+	name[1] = "Smart Buy Glossy";
+	name[2] = "Qumo Speedster";
+	name[3] = "Qumo Tropic Green";
+	name[4] = "SanDisk Cruzer Ultra Fit";
+
+	// Julia task
 
 	int j = 0;
-	for (int i = 0; i < 7; i++)
+	for (int i = 0; i < size; i++)
 		if (mat[i][0] >= krit[0])
-			step[j++] =  mat[i];
+			step[j++] = mat[i];
 
 
 	int y = 0;
-	for(int x = 1; x < 8; x++ )
+	for(int x = 1; x < size; x++ )
 	{
 		int g = 0;
 		for (int i = 0; i < j; i++)
@@ -59,17 +80,19 @@ int main()
 		while(cloak)
 		{
 			cloak = false;
-
+			
 			for (int i = 0; i < g-1; i++)
 			{
+				
 				float* temp;
 				temp = step[i+1];
-
 				if (temp[x] > step[i][x])
 				{
 					step[i+1] = step[i];
 					step[i] = temp;
 					cloak = true;
+
+					curName = name[i];
 				}
 			}
 		}
@@ -78,17 +101,14 @@ int main()
 	}
 
 
-
-	cout << "Ответ:" << endl;
-	for (int x = 0; x < j; x++){
-		for (int i = 0; i < 8; i++){
-			cout<< step[x][i]<<" ";
-		}
-		cout<<endl;
-	}
-	// cout << "Проект № " << kritFirst->getP() << endl;
-	// cout << "e = " << kritFirst->getE() << endl << endl;
-
+	cout << endl;
+	cout << "\033[33mОтвет:\033[0m " << curName << endl;
+	cout << "\033[33mПри значениях:\033[0m ";
+	for (int x = 0; x < j; x++)
+		for (int i = 0; i < size; i++)
+			if (j-1 == x)
+				cout<< step[x][i]<<" ";
+	cout<<endl;
 	return 0;
 }
 
